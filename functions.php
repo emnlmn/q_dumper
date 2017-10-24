e319977f47c826f94019f78d3102b20ea73bc704
Emanuele Menon <emanuele.menon@shellrent.com>
2017-10-24 15:19:36 +0200

    implemented dump function

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace QDM;
+
+require __DIR__.'/vendor/autoload.php';
+
+function _q( ...$args ) {
+	$backtrace = debug_backtrace();
+	$exitPoint = sprintf('exitpoint: %s@%s line %s', $backtrace[1]['class'], $backtrace[1]['function'], $backtrace[0]['line']);
+
+	foreach ( $args as $arg ) {
+		if ( is_object( $arg ) and method_exists( $arg, 'toArray' ) ) {
+			dump( $arg->toArray() );
+			continue;
+
+		} elseif ( is_object( $arg ) ) {
+			dump( (array) $arg );
+			continue;
+
+		}
+
+		dump( $arg );
+	}
+
+	dump($exitPoint);
+
+	exit();
+}
